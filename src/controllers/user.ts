a9928abc11a2ea2e771e040d5a4df8895728653c
gtole342 <gtole342@gmail.com>
2019-09-21 09:38:35 -0700

    g

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -18,73 +18,73 @@ router.get("/user", (req, res) => {
 // GET FAVORITE ARTISTS /v1/users/favoriteArtists
 router.get("/:id/favoriteArtists", (req, res) => {
   User.findById(req.params.id)
-    .then((user) => {
-      if (user) {
-        res.status(200).send(user.favoriteArtists);
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).send({ message: "Server error while attempting to find a user" });
-    });
+  .then((user) => {
+    if (user) {
+      res.status(200).send(user.favoriteArtists);
+    }
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).send({ message: "Server error while attempting to find a user" });
+  });
 });
 
 // POST FAVORITES ARTISTS /v1/users/:id/favoriteArtists/add
 router.post("/:id/favoriteArtists/add", (req, res) => {
   User.findById(req.params.id)
-    .then((user) => {
-      if (user) {
-        const faves: [{ artistId: string, postId: string }] = user.favoriteWorks;
-        faves.push(req.body.newFave);
-        user.updateOne({
-          favoriteWorks: faves,
+  .then((user) => {
+    if (user) {
+      const faves: string[] = user.favoriteArtists;
+      faves.push(req.body.artistId);
+      user.updateOne({
+        favoriteArtists: faves,
+      })
+        .then((result) => {
+          res.status(200).send(result);
         })
-          .then((result) => {
-            res.status(200).send(result);
-          })
-          .catch((err) => {
-            console.log(err);
-            res.status(500).send({ message: "Server error while attempting to add a favorite" });
-          });
-      } else {
-        res.status(500).send({ message: "User not found for adding favorites" });
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).send({ message: "Server error while attempting to find a user" });
-    });
+        .catch((err) => {
+          console.log(err);
+          res.status(500).send({ message: "Server error while attempting to add a favorite" });
+        });
+    } else {
+      res.status(500).send({ message: "User not found for adding favorites" });
+    }
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).send({ message: "Server error while attempting to find a user" });
+  });
 });
 
 // DELETE FAVORITES ARTISTS /v1/users/:id/favoritesArtists/remove
 router.delete("/:id/favoriteArtists/remove", (req, res) => {
   User.findById(req.params.id)
-    .then((user) => {
-      if (user) {
-        const faves: string[] = user.favoriteArtists;
-        const item: string = req.body.removePost;
-        const itemIndex: number = faves.indexOf(item);
-        if (itemIndex > -1) {
-          faves.splice(itemIndex, 1);
-          user.updateOne({
-            favoriteWorks: faves
-          })
-            .then(result => {
-              res.status(200).send(result);
-            })
-            .catch(err => {
-              console.log(err);
-              res.status(500).send({ message: "Server error while attempting to add a favorite" });
-            });
-        } else {
-          res.status(500).send({ message: "Couldn't find favorite to remove" });
-        }
+  .then((user) => {
+    if (user) {
+      const faves: string[] = user.favoriteArtists;
+      const item: string = req.body.artistId;
+      const itemIndex: number = faves.indexOf(item);
+      if (itemIndex > -1) {
+        faves.splice(itemIndex, 1);
+        user.updateOne({
+          favoriteWorks: faves,
+        })
+        .then((result) => {
+          res.status(200).send(result);
+        })
+        .catch((err) => {
+          console.log(err);
+          res.status(500).send({ message: "Server error while attempting to add a favorite" });
+        });
+      } else {
+        res.status(500).send({ message: "Couldn't find favorite to remove" });
       }
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).send({ message: "Server error while attempting to find a user" });
-    });
+    }
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).send({ message: "Server error while attempting to find a user" });
+  });
 });
 
 export default router;
