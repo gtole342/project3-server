fd23c8b1982add9046a19cfe2c661d9dd58aee11
gtole342 <gtole342@gmail.com>
2019-09-21 14:51:34 -0700

    g

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ import user from "./controllers/user";
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
 app.use(cors());
 dotenv.config();
 
