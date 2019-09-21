546da09b5728f712db734f0fa91771f6f16775c7
gtole342 <gtole342@gmail.com>
2019-09-20 23:32:56 -0700

    d

--- src/models/vendor.ts
+++ src/models/vendor.ts
@@ -37,6 +37,7 @@ VendorSchema.pre<IVendorModel>("save", async function(next) {
       const facebookIdPageUrl = `https://graph.facebook.com/v4.0/me/accounts?access_token=${this.instagramAccessToken}`;
       axios.get(facebookIdPageUrl)
       .then((response) => {
+        console.log(response.data.data.id)
         const pageId = response.data.data.id;
         const instagramIdPageUrl = `https://graph.facebook.com/v4.0/${pageId}?fields=instagram_business_account&access_token=${this.instagramAccessToken}`;
         axios.get(instagramIdPageUrl)
