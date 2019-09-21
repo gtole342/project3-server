e0668b50a9fa0cdb7728ccd474784612ee83ef9d
gtole342 <gtole342@gmail.com>
2019-09-21 00:02:40 -0700

    d

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -46,7 +46,7 @@ router.post("/signup", (req, res) => {
       },
     })
     .then((newUser) => {
-      console.log(newUser);
+      console.log(newUser," new user");
       const token = jwt.sign(newUser.toJSON(), process.env.JWT_SECRET || "", {
         expiresIn: 60 * 60 * 8,
       });
